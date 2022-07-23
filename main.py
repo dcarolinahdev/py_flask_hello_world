ec5fac553bc3d57d1bd6069af019f39e96636f93
Diana Carolina Hernandez <dianacaro3552@gmail.com>
2022-07-23 13:12:49 -0500

    Templates
    Templates v0.2 using conditionals
    
    Templates v0.3 using for loop
    
    Templates v0.4 using template inheritance
    
    Templates v0.5 using a macro
    
    Templates v0.6 include other template
    
    Templates v0.7 using static files (img and css)
    
    Templates v0.8 error

--- main.py
+++ main.py
@@ -2,6 +2,21 @@ from flask import Flask, request, make_response, redirect, render_template
 
 app = Flask(__name__)
 
+todos = [
+    'Take a breakfast',
+    'Take my kid to kindergarden',
+    'Take a lunch',
+    'Bring my kid from kindergarden',
+    'Take a family time']
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html', error=error)
+
+@app.errorhandler(500)
+def internal_server(error):
+    return render_template('500.html', error=error)
+
 @app.route('/')
 def index():
     user_ip = request.remote_addr
@@ -13,4 +28,8 @@ def index():
 @app.route('/hello')
 def hello():
     user_ip = request.cookies.get('user_ip')
-    return 'Hello World Platzi! Your IP is {}'.format(user_ip)
+    context = {
+        'user_ip': user_ip,
+        'todos':todos,
+    }
+    return render_template('hello.html', **context)
