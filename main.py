b0c749ae5e9c03feb183ebf0f8230e4ab41ae7c4
Diana Carolina Hernandez <dianacaro3552@gmail.com>
2022-07-23 13:10:40 -0500

    Request and response with cookies

--- main.py
+++ main.py
@@ -3,6 +3,14 @@ from flask import Flask, request, make_response, redirect, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
+def index():
     user_ip = request.remote_addr
-    return 'Hello World! Your IP is {}'.format(user_ip)
+
+    response = make_response(redirect('/hello'))
+    response.set_cookie('user_ip', user_ip)
+    return response
+
+@app.route('/hello')
+def hello():
+    user_ip = request.cookies.get('user_ip')
+    return 'Hello World Platzi! Your IP is {}'.format(user_ip)
