236f96a27fec537025c0810df02f3c8c7759d5c3
Diana Carolina Hernandez <dianacaro3552@gmail.com>
2022-07-25 11:28:32 -0500

    Updating cookies to session

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from ensurepip import bootstrap
-from flask import Flask, request, make_response, redirect, render_template
+from flask import Flask, request, make_response, redirect, render_template, session
 from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+
+app.config['SECRET_KEY'] = 'h7uyG65gv67BHgbVbjB'
 
 todos = [
     'Take a breakfast',
@@ -25,12 +27,12 @@ def index():
     user_ip = request.remote_addr
 
     response = make_response(redirect('/hello'))
-    response.set_cookie('user_ip', user_ip)
+    session['user_ip'] = user_ip
     return response
 
 @app.route('/hello')
 def hello():
-    user_ip = request.cookies.get('user_ip')
+    user_ip = session.get('user_ip')
     context = {
         'user_ip': user_ip,
         'todos':todos,
