ea8b09485956fecfc954459d2bd81d42ced0be9a
Diana Carolina Hernandez <dianacaro3552@gmail.com>
2022-07-25 11:53:21 -0500

    WTForms: setting, basic validation and user data in session

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-from ensurepip import bootstrap
-from flask import Flask, request, make_response, redirect, render_template, session
+from flask import Flask, request, make_response, redirect, render_template, session, url_for
 from flask_bootstrap import Bootstrap
+
+from flask_wtf import FlaskForm
+from wtforms.fields import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
@@ -13,6 +16,11 @@ todos = [
     'Take a lunch',
     'Bring my kid from kindergarden',
     'Take a family time']
+
+class LoginForm(FlaskForm):
+    username = StringField('Nombre de usuario', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Enviar')
 
 @app.errorhandler(404)
 def not_found(error):
@@ -30,11 +38,21 @@ def index():
     session['user_ip'] = user_ip
     return response
 
-@app.route('/hello')
+@app.route('/hello', methods=['GET', 'POST'])
 def hello():
     user_ip = session.get('user_ip')
+    login_form = LoginForm()
+    username = session.get('username')
     context = {
         'user_ip': user_ip,
         'todos':todos,
+        'login_form': login_form,
+        'username':username,
     }
+
+    if login_form.validate_on_submit():
+        username = login_form.username.data
+        session['username'] = username
+        return redirect(url_for('index'))
+
     return render_template('hello.html', **context)
